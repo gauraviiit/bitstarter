e183c1bac574941ae42603ddb1b895fbb3471a43
Gaurav Sharma <gaurav.sharma.1991@gmail.com>
2013-07-12 18:11:37 +0000

    Update.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var buf = new Buffer(20);
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  buf=data;
+  });
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
